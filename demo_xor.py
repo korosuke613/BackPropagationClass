15b244177faaf2b8794828a91c5c751c9f299565
Futa HIRAKOBA <korosuke613@users.noreply.github.com>
2018-05-30 19:17:48 +0900

    finish demo_xor

--- demo_xor.py
+++ demo_xor.py
@@ -1,5 +1,45 @@
 from neural_network import NeuralNetwork
 from activation import Sigmoid
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+
+
+class NnXor(NeuralNetwork):
+    def __init__(self, ni, nh, no, pattern, activator=Sigmoid, title='none'):
+        super().__init__(ni, nh, no, activator, title)
+        self.pattern = pattern
+
+    def draw(self):
+        def draw_pattern():
+            for i in [(0, 'red'), (1, 'blue')]:
+                x_set = [x[0][0] for x in self.pattern if x[1][0] == i[0]]
+                y_set = [y[0][1] for y in self.pattern if y[1][0] == i[0]]
+                plt.scatter(x_set, y_set, c=i[1])
+
+        def draw_surface():
+            def generate_all_patterns():
+                x_set = [x for x in range(-50, 150)]
+                y_set = [y for y in range(-50, 150)]
+                learn_data = [[x / 100, y / 100] for x in x_set for y in y_set]
+                return learn_data
+            patterns = generate_all_patterns()
+            result = []
+            for p in tqdm(patterns):
+                result.append(self.update(p))
+            draw_p = [p for i, p in enumerate(patterns) if result[i][0] <= 0.4]
+            draw_x = [x[0] for x in draw_p]
+            draw_y = [y[1] for y in draw_p]
+            plt.scatter(draw_x, draw_y, marker='.', c='coral', s=0.1)
+            draw_p = [p for i, p in enumerate(patterns) if result[i][0] >= 0.6]
+            draw_x = [x[0] for x in draw_p]
+            draw_y = [y[1] for y in draw_p]
+            plt.scatter(draw_x, draw_y, marker='.', c='aqua', s=0.1)
+
+        plt.title(self.get_plt_title())
+        draw_surface()
+        draw_pattern()
+        plt.savefig(self.timestamp + '.draw.pdf')
+        plt.show()
 
 
 def demo():
@@ -12,10 +52,11 @@ def demo():
     ]
 
     # create a network with two input, two hidden, and one output nodes
-    n = NeuralNetwork(2, 2, 1, activator=Sigmoid, title='XOR')
+    n = NnXor(2, 2, 1, pat, activator=Sigmoid, title='XOR')
     # train it with some patterns
     n.train(pat, epoch=1000)
     n.print_error(is_graph=True)
+    n.draw()
     # test it
     n.test(pat)
 
