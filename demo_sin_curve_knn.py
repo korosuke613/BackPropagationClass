6e96408593e6b0b6b124cd12d19cbfa7ddb2cfb4
Futa Hirakoba <korosuke613613@gmail.com>
2018-06-30 11:33:54 +0900

    add calc bias variance error

--- demo_sin_curve_knn.py
+++ demo_sin_curve_knn.py
@@ -2,6 +2,8 @@ from demo_sin_curve import NnSin
 import math
 import random_tool
 import operator
+import matplotlib.pyplot as plt
+from datetime import datetime
 
 
 class NnSinKnn(NnSin):
@@ -9,6 +11,9 @@ class NnSinKnn(NnSin):
         super().__init__(2, 16, 1, pattern=pattern, title=title, fineness=fineness)
         self.pattern = pattern
         self.k = k
+        self.bias = None
+        self.variance = None
+        self.error = None
 
     def update(self, inputs):
         euclid = []
@@ -28,23 +33,56 @@ class NnSinKnn(NnSin):
         return [0.5]
 
     def test(self, patterns):
+        def calc_bias(di):
+            r = 0
+            for d in di:
+                r += d
+            return (r / 100) ** 2
+
+        def calc_varience(di):
+            r = 0
+            fx = 0
+            for d in di:
+                fx += d
+            fx /= 100
+            for d in di:
+                r += (d - fx) ** 2
+            return r / 100
+
+        def calc_error(di, pat):
+            r = 0
+            for i, d in enumerate(di):
+                r += (d - pat[i][1][0]) ** 2
+            return r / 100
         correct = 0
+        diffs = []
+        fx = []
         for p in patterns:
             result = self.update(p[0])
-            print(p[0], '->', result)
-            if abs(result[0] - p[1][0]) < 0.05:
+            #print(p[0], '->', result)
+            diff = abs(result[0] - p[1][0])
+            diffs.append(p[0][1] - result[0])
+            fx.append(result[0])
+            if diff < 0.05:
                 correct += 1
+        self.bias = round(calc_bias(diffs), 5)
+        self.variance = round(calc_varience(fx), 5)
+        self.error = round(calc_error(fx, patterns), 5)
         self.accuracy = correct / len(patterns)
-        print("accuracy: {:.2%}".format(self.accuracy))
+        print(f"accuracy: {self.accuracy:.2%}\n"
+              f'error: {self.error}\n'
+              f'bias: {self.bias}\n'
+              f'variance: {self.variance}\n')
 
     def get_plt_title(self):
         activation = self.activator.__name__
         return f'title:{self.title}, \n' \
                f'perceptron=[{self.ni-1}, {self.nh-1}, {self.no}], k={self.k}\n' \
-               f'activation={activation}, accuracy={self.accuracy*100}%'
+               f'activation={activation}, accuracy={self.accuracy*100}%\n' \
+               f'error={self.error}, bias={self.bias}, variance={self.variance}'
 
 
-def demo_sin_curve_knn():
+def demo_sin_curve_knn(num, isDraw=True):
     def generate_leaning_data():
         learn_data = []
         up_num = down_num = 0
@@ -69,12 +107,38 @@ def demo_sin_curve_knn():
 
     pat = generate_leaning_data()
 
-    n = NnSinKnn(pat, k=3, title='sin_curve_with_kNN', fineness=25)
+    n = NnSinKnn(pat, k=num, title='sin_curve_with_kNN', fineness=25)
     # n.activation = tanh
     # n.dactivation = dtanh
     n.test(pat)
-    n.draw()
+    if isDraw:
+        n.draw()
+    return n.bias, n.variance, n.error
+
+
+def create_bve():
+    bias = []
+    variance = []
+    error = []
+    for i in range(1, 11):
+        print(f"k={i}")
+        b = v = e = 0
+        for j in range(100):
+            b_, v_, e_ = demo_sin_curve_knn(i, isDraw=False)
+            b += b_
+            v += v_
+            e += e_
+        bias.append(b / 100)
+        variance.append(v / 100)
+        error.append(e / 100)
+    plt.plot(bias)
+    plt.plot(variance)
+    plt.plot(error)
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+    plt.savefig('fig/' + timestamp + '_' + "bias_variance" + '.draw.png')
+    plt.show()
 
 
 if __name__ == '__main__':
-    demo_sin_curve_knn()
+    create_bve()
+    #demo_sin_curve_knn(4)
